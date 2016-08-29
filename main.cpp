600552cffceb2346bad8e4c597d85a6c29a8b7ac
AfroMonkey <moisalejandro@gmail.com>
2016-08-28 19:33:40 -0500

    [IMP] merge

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "merge_sort.hpp"
 
-#define NUM 50000
+#define NUM 5000
 
 int compare(const void* a, const void* b)
 {
@@ -14,19 +14,10 @@ int main(int argc, char const *argv[])
     int array[NUM];
     for (int i = 0; i < NUM; i++)
     {
-        array[NUM] = std::rand();
+        array[i] = std::rand();
     }
     std::cout << "Start" << std::endl;
     afro_merge(array, NUM, sizeof(int), compare);
-    for (int i = 1; i < NUM; i++) // TODO [REM]
-    {
-        if (array[i] < array[i-1])
-        {
-            std::cout << "Error" << std::endl;
-            std::cout << array[i-1] << "<" << array[i] << std::endl;
-            break;
-        }
-    }
     std::cout << "End" << std::endl;
     return 0;
 }
--- merge_sort.hpp
+++ merge_sort.hpp
@@ -6,15 +6,54 @@
 void afro_swap(void* a, void* b, size_t size)
 {
     char *c = (char*)malloc(size);
-    std::memcpy(c,a,size);
-    std::memcpy(a,b,size);
-    std::memcpy(b,c,size);
+    memcpy(c,a,size);
+    memcpy(a,b,size);
+    memcpy(b,c,size);
     free(c);
+}
+
+void _merge(void* base, void* left, void* right, size_t middle, size_t middle2, size_t size, int((*compare)(const void*, const void*)))
+{
+    size_t i, j, k;
+    for (i = 0, j = 0, k = 0; i < middle && j < middle2; k++)
+    {
+        if (compare((char*)left + i * size, (char*)right + j * size) < 0)
+        {
+            memcpy((char*)base + k * size, (char*)left + i * size, size);
+            i++;
+        }
+        else
+        {
+            memcpy((char*)base + k * size, (char*)right + j * size, size);
+            j++;
+        }
+    }
+    if (i < middle)
+    {
+        memcpy((char*)base + k * size, (char*)left + i * size, (middle - i) * size);
+    }
+    if (j < middle2)
+    {
+        memcpy((char*)base + k * size, (char*)right + j * size, (middle2 - j) * size);
+    }
 }
 
 void afro_merge(void* base, size_t num, size_t size, int((*compare)(const void*, const void*)))
 {
-    // TODO
+    if (num > 1)
+    {
+        size_t middle = num / 2;
+        size_t middle2 = num - middle;
+        void* left = (void*)malloc(middle * size);
+        void* right = (void*)malloc(middle2 * size);
+        memcpy(left, base, middle * size);
+        memcpy(right, (char*)base + middle * size, middle2 * size);
+        afro_merge(left, middle, size, compare);
+        afro_merge(right, num - middle, size, compare);
+        _merge(base, left, right, middle, middle2, size, compare);
+        free(left);
+        free(right);
+    }
 }
 
 #endif
